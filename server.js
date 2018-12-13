94ac055a2d3b573909fb0d53a6e7afaa13d0ead4
Glitch (hello-express) <none>
2018-12-13 12:55:54 +0000

    👔👟 Checkpoint ./server.js:66749/1284

--- server.js
+++ server.js
@@ -8,12 +8,36 @@ const app = express();
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
+//TODO GET ALL FILES IN ZIPS FODLER
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
+});
+
+//for zip upload
+app.post('/upload', function(request, response) {
+  /*
+  1. Receive bytes
+  2. Write to file to be wiped
+  */
+});
+
+//for giving token, repo url, and zip receipt
+app.post('/api', function(request, response) {
+  /*
+  1. Get git tokens
+  2. Get git url
+  3. Make quarantine folder w/ unique case ID
+  4. Clone git repo
+  5. Make subfolder and unzip
+  6. Commit
+  7. Push
+  */
+  console.log(requests.body);
 });
 
 // listen for requests :)
